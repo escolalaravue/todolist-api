d11f23ad2af7e6782b1440eb2b9942bc99046ad5
Tiago Matos <tiago@tiagomatos.com>
2020-10-18 17:02:21 +1100

    Add new endpoint for creating a new todo

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TodoStoreRequest;
 use App\Http\Resources\TodoResource;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -22,5 +23,17 @@ class TodoController extends Controller
     public function index()
     {
          return TodoResource::collection(auth()->user()->todos);
+    }
+
+    /**
+     * @param TodoStoreRequest $request
+     * @return TodoResource
+     */
+    public function store(TodoStoreRequest $request)
+    {
+        $input = $request->validated();
+        $todo = auth()->user()->todos()->create($input);
+
+        return new TodoResource($todo);
     }
 }

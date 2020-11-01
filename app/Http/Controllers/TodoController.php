0791558dcecdfb798a854dedaae5f99f669b3e05
Tiago Matos <tiago@tiagomatos.com>
2020-11-01 14:21:45 +1100

    Add endpoint to add tasks

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoStoreRequest;
+use App\Http\Requests\TodoTaskStoreRequest;
 use App\Http\Requests\TodoUpdateRequest;
 use App\Http\Resources\TodoResource;
+use App\Http\Resources\TodoTaskResource;
 use App\Todo;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
@@ -71,5 +73,18 @@ class TodoController extends Controller
     public function destroy(Todo $todo)
     {
         $todo->delete();
+    }
+
+    /**
+     * @param Todo $todo
+     * @param TodoTaskStoreRequest $request
+     * @return TodoTaskResource
+     */
+    public function addTask(Todo $todo, TodoTaskStoreRequest $request)
+    {
+        $input = $request->validated();
+        $todoTask = $todo->tasks()->create($input);
+
+        return new TodoTaskResource($todoTask);
     }
 }
